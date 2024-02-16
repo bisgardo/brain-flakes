63ab92fc1f99075152c3c771e54a907174ca7a02
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2024-02-16 23:35:34 +0100

    app: Read program from file
    The application expects a single CLI argument with the path of the file containing the Brainfuck program.

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,17 @@
 module Main where
 
-import Instruction
+import Control.Monad (when)
+import Instruction (parse)
+import System.Environment (getArgs)
+import System.Exit (die)
 
 main :: IO ()
 main = do
-  input <- getContents
+  args <- getArgs
+  when (null args) $
+    die "Error: No command-line arguments provided."
+  let path = args !! 0
+  input <- readFile path
   let instrs = parse (1, 1) input
   putStrLn "Instructions:"
   putStrLn $ show instrs
